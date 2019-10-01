bc3463a8e0a353be4af27f30f5677f4429de3f71
Abraham Moreno <adiaz2821@gmail.com>
2019-10-01 11:30:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,9 +5,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  scale = [2,3,4,5,]
+  
 end
 
 def find_min_value(array)
-  # Add your solution here
+  scale = [2,3,4,5]
+  
 end
